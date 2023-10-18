01bea7ce6c71256e6a0939fa57410b7c3f799113
movingfastball <movingfastballny2012@yahoo.co.jp>
2023-10-18 11:26:09 +0900

    2章記録

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';//1
+import ReactDOM from 'react-dom/client';//1
+import './index.css';//2
+import App from './App';//2
+import reportWebVitals from './reportWebVitals';//3
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+const root = ReactDOM.createRoot(document.getElementById('root'));//ここから4
+
+const dest = 'https://ja.react.dev';
+
+  root.render(
+    <p className="hoge">WINGSプロジェクト</p>
+  )
+
+
+//ここまで4
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();//5
